4011bd989e433e3a3e82711197ab15209ab4d1dd
Nicholas Crispino <crispinonicky@gmail.com>
2019-11-25 09:41:38 -0500

    cors working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const corsAnywhere = 'https://cors-anywhere.herokuapp.com/'
+  console.log("App is running")
+
+  // This functions gets all of the information on a user:
+  function getUser(user){
+    axios.get(corsAnywhere + 'https://www.codewars.com/api/v1/users/' + user)
+    .then(data => {
+      console.log(data.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  getUser('ncrispino')
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
