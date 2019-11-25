ed1c7d6332370d2426cb5e83975c572ccd16255e
Nicholas Crispino <crispinonicky@gmail.com>
2019-11-25 14:56:36 -0500

    cleaned up code and minor styling

--- src/App.js
+++ src/App.js
@@ -1,153 +1,149 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import React, { useState, useEffect } from "react";
 
-
 function App() {
-
-  // const [inputs, setInput] = useState();
   const [userInfo, setUserInfo] = useState("");
   const [userChallenges, setUserChallenges] = useState("");
   const [challengeInfo, setChallengeInfo] = useState("");
 
   useEffect(() => {
-    console.log('User we are searching for (INFO): ',userInfo)
-    console.log('User we are searching for (CHALLENGES): ', userChallenges)
-    console.log('Challenge we are searching for: ', challengeInfo)
-
+    // console.log("User we are searching for (INFO): ", userInfo);
+    // console.log("User we are searching for (CHALLENGES): ", userChallenges);
+    // console.log("Challenge we are searching for: ", challengeInfo);
   });
 
-  let sampleUser = 'ncrispino'
+  const corsAnywhere = "https://cors-anywhere.herokuapp.com/";
 
-  const corsAnywhere = 'https://cors-anywhere.herokuapp.com/'
-  console.log("App is running")
-
-  // This functions gets all of the information on a user:
-  function getUser(e, user){
-    user = userInfo
-    axios.get(corsAnywhere + 'https://www.codewars.com/api/v1/users/' + user)
-    .then(data => {
-      console.log(`This is the information on ${userInfo}`)
-      console.log("Here is all of the information on the user")
-      console.log(data.data)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-    console.log(userInfo)
-    e.preventDefault()
+  // This function gets all of the information on a user:
+  function getUser(e, user) {
+    user = userInfo;
+    axios
+      .get(corsAnywhere + "https://www.codewars.com/api/v1/users/" + user)
+      .then(data => {
+        console.log(`This is the information on ${userInfo}`);
+        console.log(data.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    console.log("User we are searching for (INFO): ", userInfo);
+    e.preventDefault();
   }
 
-  // getUser(sampleUser)
+  // This function returns an array of all the challenges
+  // that a user has completed
 
-  function getCompleted(e, user){
-    user = userChallenges
-    console.log(user)
-    axios.get(corsAnywhere + 'https://www.codewars.com/api/v1/users/' + user + '/code-challenges/completed?page=0')
-    .then(data => {
-      console.log("Here are the challenges that the user has completed:")
-      console.log(data.data.data)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-    e.preventDefault()
+  function getCompleted(e, user) {
+    user = userChallenges;
+    axios
+      .get(
+        corsAnywhere +
+          "https://www.codewars.com/api/v1/users/" +
+          user +
+          "/code-challenges/completed?page=0"
+      )
+      .then(data => {
+        console.log(`Here are the challenges that ${user} has completed:`);
+        console.log(data.data.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    console.log("User we are searching for (CHALLENGES): ", userChallenges);
+    e.preventDefault();
   }
 
-  // getCompleted(sampleUser)
+  // Skipping authored challenges because very few students
+  // have created challenges of their own
 
-  // Skipping authored challenges
+  // Gives information on a codewars kata. Can accept either a
+  // title formatted-like-this or an ID
 
-  // Can be an ID, or the name of a challenge formatted-like-this
-  let sampleChallenge = '5277c8a221e209d3f6000b56'
-
-  function getChallenge(e, challenge){
-    challenge = challengeInfo
-    axios.get(corsAnywhere + 'https://www.codewars.com/api/v1/code-challenges/' + challenge)
-    .then(data => {
-      console.log("Here is the information of a single challenge:")
-      console.log(data.data)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-    e.preventDefault()
+  function getChallenge(e, challenge) {
+    challenge = challengeInfo;
+    axios
+      .get(
+        corsAnywhere +
+          "https://www.codewars.com/api/v1/code-challenges/" +
+          challenge
+      )
+      .then(data => {
+        console.log(`Here is the information on ${challenge}:`);
+        console.log(data.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    console.log("Challenge we are searching for: ", challengeInfo);
+    e.preventDefault();
   }
 
-  // getChallenge(sampleChallenge)
-
-  // function test() {
-  //   console.log('function is connected')
-  // }
-
-  function handleChangeUser(e){
-    setUserInfo(e.target.value)
+  function handleChangeUser(e) {
+    setUserInfo(e.target.value);
   }
 
-  function handleChangeUserChallenges(e){
-    setUserChallenges(e.target.value)
+  function handleChangeUserChallenges(e) {
+    setUserChallenges(e.target.value);
   }
 
-  function handleChangeChallenge(e){
-    setChallengeInfo(e.target.value)
+  function handleChangeChallenge(e) {
+    setChallengeInfo(e.target.value);
   }
-  
-  // function handleSubmit(e){
-  //   console.log("Submitted")
-  //   e.preventDefault()
-  // }
-
-
 
   return (
     <div className="App">
-
-    {/* <form onSubmit={getUser}>
-        <label>
-          Name:
-          <input type="text" value={userInfo} onChange={handleChangeUser} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form> */}
-
-
       <ul>
         <li>
+          <form onSubmit={getUser}>
+            <label>
+              Enter a user to show their info:
+              <input type="text" value={userInfo} onChange={handleChangeUser} />
+            </label>
+            <input
+              type="submit"
+              value="Submit"
+              onClick={e => getUser(e, { userInfo })}
+            />
+          </form>
+        </li>
 
-        <form onSubmit={getUser}>
-        <label>
-          Enter a user:
-          <input type="text" value={userInfo} onChange={handleChangeUser} />
-        </label>
-        <input type = "submit" value = "Submit" onClick ={(e) => getUser(e, {userInfo})}/>
-      </form>
+        <li>
+          <form onSubmit={getCompleted}>
+            <label>
+              Enter a user to show their completed challenges:
+              <input
+                type="text"
+                value={userChallenges}
+                onChange={handleChangeUserChallenges}
+              />
+            </label>
+            <input
+              type="submit"
+              value="Submit"
+              onClick={e => getCompleted(e, { userChallenges })}
+            />
+          </form>
+        </li>
 
-          </li>
-
-          <li>
-
-        <form onSubmit={getCompleted}>
-        <label>
-          Enter a user here too:
-          <input type="text" value={userChallenges} onChange={handleChangeUserChallenges} />
-        </label>
-        <input type = "submit" value = "Submit" onClick ={(e) => getCompleted(e, {userChallenges})}/>
-      </form>
-          </li>        
-          
-          <li>
-
-        <form onSubmit={getChallenge}>
-        <label>
-          Enter a challenge here (name or ID):
-          <input type="text" value={challengeInfo} onChange={handleChangeChallenge} />
-        </label>
-        <input type = "submit" value = "Submit" onClick ={(e) => getChallenge(e, {challengeInfo})}/>
-      </form>
-          </li>        
-          </ul>
-
+        <li>
+          <form onSubmit={getChallenge}>
+            <label>
+              Enter a challenge here (name or ID) to show its stats:
+              <input
+                type="text"
+                value={challengeInfo}
+                onChange={handleChangeChallenge}
+              />
+            </label>
+            <input
+              type="submit"
+              value="Submit"
+              onClick={e => getChallenge(e, { challengeInfo })}
+            />
+          </form>
+        </li>
+      </ul>
     </div>
   );
 }
