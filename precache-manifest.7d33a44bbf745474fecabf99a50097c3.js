c94748f96ac726e9065cc651683fb4238d070884
Nicholas Crispino <crispinonicky@gmail.com>
2019-11-25 16:20:40 -0500

    Updates

---
+++ precache-manifest.7d33a44bbf745474fecabf99a50097c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea065060447a2b6baf2cfed9e36e1b2",
+    "url": "/codewars-api/index.html"
+  },
+  {
+    "revision": "b288235ead98d5148a95",
+    "url": "/codewars-api/static/css/main.81366e74.chunk.css"
+  },
+  {
+    "revision": "9305b61ebb8632d0499f",
+    "url": "/codewars-api/static/js/2.46f48d7c.chunk.js"
+  },
+  {
+    "revision": "b288235ead98d5148a95",
+    "url": "/codewars-api/static/js/main.c831ee1e.chunk.js"
+  },
+  {
+    "revision": "682f197f8a2f532a89b9",
+    "url": "/codewars-api/static/js/runtime-main.14561675.js"
+  }
+]);
