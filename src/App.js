cd4d84bda08f520bd4b4423ba897eb9a147a3cc2
Nicholas Crispino <crispinonicky@gmail.com>
2019-11-25 13:37:36 -0500

    basic functonality achieved

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import React, { useState, useEffect } from "react";
 
 function App() {
 
-  const [inputs, setInput] = useState();
+  // const [inputs, setInput] = useState();
   const [userInfo, setUserInfo] = useState("");
   const [userChallenges, setUserChallenges] = useState("");
   const [challengeInfo, setChallengeInfo] = useState("");
 
   useEffect(() => {
-    console.log(userInfo)
-    console.log(userChallenges)
+    console.log('User we are searching for (INFO): ',userInfo)
+    console.log('User we are searching for (CHALLENGES): ', userChallenges)
+    console.log('Challenge we are searching for: ', challengeInfo)
+
   });
 
   let sampleUser = 'ncrispino'
@@ -39,7 +41,9 @@ function App() {
 
   // getUser(sampleUser)
 
-  function getCompleted(user){
+  function getCompleted(e, user){
+    user = userChallenges
+    console.log(user)
     axios.get(corsAnywhere + 'https://www.codewars.com/api/v1/users/' + user + '/code-challenges/completed?page=0')
     .then(data => {
       console.log("Here are the challenges that the user has completed:")
@@ -48,6 +52,7 @@ function App() {
     .catch(err => {
       console.log(err)
     })
+    e.preventDefault()
   }
 
   // getCompleted(sampleUser)
@@ -57,7 +62,8 @@ function App() {
   // Can be an ID, or the name of a challenge formatted-like-this
   let sampleChallenge = '5277c8a221e209d3f6000b56'
 
-  function getChallenge(challenge){
+  function getChallenge(e, challenge){
+    challenge = challengeInfo
     axios.get(corsAnywhere + 'https://www.codewars.com/api/v1/code-challenges/' + challenge)
     .then(data => {
       console.log("Here is the information of a single challenge:")
@@ -66,6 +72,7 @@ function App() {
     .catch(err => {
       console.log(err)
     })
+    e.preventDefault()
   }
 
   // getChallenge(sampleChallenge)
@@ -80,6 +87,10 @@ function App() {
 
   function handleChangeUserChallenges(e){
     setUserChallenges(e.target.value)
+  }
+
+  function handleChangeChallenge(e){
+    setChallengeInfo(e.target.value)
   }
   
   // function handleSubmit(e){
@@ -103,30 +114,39 @@ function App() {
 
       <ul>
         <li>
+
         <form onSubmit={getUser}>
         <label>
           Enter a user:
           <input type="text" value={userInfo} onChange={handleChangeUser} />
         </label>
-        <input type = "submit" value = "Get user info" onClick ={(e) => getUser(e, {userInfo})}/>
-
-        {/* <input type="submit" value="Submit" /> */}
+        <input type = "submit" value = "Submit" onClick ={(e) => getUser(e, {userInfo})}/>
       </form>
-          {/* <button type = "submit" onClick ={(e) => getUser({userInfo})}>Get the user info</button> */}
+
           </li>
+
           <li>
+
         <form onSubmit={getCompleted}>
         <label>
           Enter a user here too:
           <input type="text" value={userChallenges} onChange={handleChangeUserChallenges} />
         </label>
-        <input type = "submit" value = "Get the challenges the user has completed" onClick ={(e) => getChallenge(e, {userChallenges})}/>
-
-        {/* <input type="submit" value="Submit" /> */}
+        <input type = "submit" value = "Submit" onClick ={(e) => getCompleted(e, {userChallenges})}/>
       </form>
-          {/* <button type = "submit" onClick ={(e) => getUser({userInfo})}>Get the user info</button> */}
-          </li>        <li><button onClick ={(e) => getChallenge('5277c8a221e209d3f6000b56')}>Get the challenge info</button></li>
-      </ul>
+          </li>        
+          
+          <li>
+
+        <form onSubmit={getChallenge}>
+        <label>
+          Enter a challenge here (name or ID):
+          <input type="text" value={challengeInfo} onChange={handleChangeChallenge} />
+        </label>
+        <input type = "submit" value = "Submit" onClick ={(e) => getChallenge(e, {challengeInfo})}/>
+      </form>
+          </li>        
+          </ul>
 
     </div>
   );
